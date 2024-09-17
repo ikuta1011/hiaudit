ff5de6cfd01cf977f85c52164dad3f78ef3290bf
ikuta1011 <ikuta1011@gmail.com>
2024-09-18 00:29:34 +0900

    Update PVX_ERC1155mint.sol

--- PVX_ERC1155mint.sol
+++ PVX_ERC1155mint.sol
@@ -1,14 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-
 
 contract PVX_ERC1155mint is ERC1155, ERC1155Supply, Ownable, Pausable {
     using EnumerableSet for EnumerableSet.UintSet;
