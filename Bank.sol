8b2da4ee9d58ad7a0782aff8eaae42ec068aa5f2
ikuta1011 <ikuta1011@gmail.com>
2024-11-13 17:12:23 +0900

    Add files via upload

---
+++ Bank.sol
@@ -0,0 +1,29 @@
+contract Private_Bank {
+    mapping(address => uint256) public balances;
+
+    uint256 public MinDeposit = 1 ether;
+
+    Log TransferLog;
+
+    function Private_Bank(address _log) {
+        TransferLog = Log(_log);
+    }
+
+    function Deposit() public payable {
+        if (msg.value >= MinDeposit) {
+            balances[msg.sender] += msg.value;
+            TransferLog.AddMessage(msg.sender, msg.value, 'Deposit');
+        }
+    }
+
+    function CashOut(uint256 _am) {
+        if (_am <= balances[msg.sender]) {
+            if (msg.sender.call.value(_am)()) {
+                balances[msg.sender] -= _am;
+                TransferLog.AddMessage(msg.sender, _am, 'CashOut');
+            }
+        }
+    }
+
+    function() public payable {}
+}
